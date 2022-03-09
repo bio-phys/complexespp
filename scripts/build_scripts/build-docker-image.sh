ce90b86267ff546a9d5045dc8f2ac45adb620409
Max Linke <max.linke88@gmail.com>
2022-03-09 22:07:54 +0100

    add complexespp ci

--- scripts/build_scripts/build-docker-image.sh
+++ scripts/build_scripts/build-docker-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# needs to be run as root for docker privileges
-docker build --tag=biophys/complexespp:builder-debian --file=Dockerfile.debian9 .
-docker build --tag=biophys/complexespp:builder-centos --file=Dockerfile.centos7 .
+# needs to be run as root for docker privileges, or use podman
+docker build --tag=malinke/complexesbuilder:debian --file=Dockerfile.debian11 .
+#docker build --tag=biophys/complexespp:builder-centos --file=Dockerfile.centos7 .
