81a12fff5a43ed6ead7ea82c85e26680912232c5
Max Linke <max.linke88@gmail.com>
2022-04-30 13:09:24 +0200

    remove unused scripts

--- scripts/build_scripts/build-docker-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# needs to be run as root for docker privileges, or use podman
-docker build --tag=malinke/complexesbuilder:debian --file=Dockerfile.debian11 .
-#docker build --tag=biophys/complexespp:builder-centos --file=Dockerfile.centos7 .
